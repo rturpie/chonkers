10e50787d628a35ae63a9711f3c0ea1a02ff8879
t_ruth_less <s1645316@ed.ac.uk>
2020-03-01 12:20:43 +0000

    server commit

--- server/app/models.py
+++ server/app/models.py
@@ -5,13 +5,21 @@ from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+    username    = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    location = db.Column(db.Integer, db.ForeignKey('location.id'))
-    health = db.Column(db.Integer)
-    max_health = db.Column(db.Integer)
+    location    = db.Column(db.Integer, db.ForeignKey('location.id'))
+    health      = db.Column(db.Integer)
+    max_health  = db.Column(db.Integer)
     last_active = db.Column(db.DateTime)
-    position = db.Column(db.String(32))
+    position    = db.Column(db.String(32))
+    is_alive    = db.Column(db.Boolean)
+    direction   = db.Column(db.String(20))
+    action      = db.Column(db.String(30))
+    ready       = db.Column(db.Boolean)
+    xp          = db.Column(db.Integer)
+    hunger      = db.Column(db.Integer)
+    max_hunger  = db.Column(db.Integer)
+    level       = db.Column(db.Integer)
 
     def __repr__(self):
         return '<User {}>'.format(self.username) 
@@ -24,12 +32,21 @@ class User(UserMixin, db.Model):
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), unique=True)
+    name = db.Column(db.String(128))
+    init_pos = db.Column(db.String(20))
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), index=True, unique=True)
     description = db.Column(db.String(1024))
+
+class Entity(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(256))
+    type   = db.Column(db.String(20))
+    health = db.Column(db.Integer)
+    location = db.Column(db.Integer)
+    position = db.Column(db.String(20))
 
 @login.user_loader
 def load_user(id):
